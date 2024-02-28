b2575bfd981025b77cd3424dbeecf9e81e25c017
Andrew Jarrett <ahrjarrett@gmail.com>
2024-02-28 06:52:50 -0600

    bump: v0.34.4

--- src/any.ts
+++ src/any.ts
@@ -303,7 +303,7 @@ declare namespace distributive {
 namespace any_ {
   export type PKG_VERSION = typeof PKG_VERSION
   // TODO: generate this identifier from manifest
-  export const PKG_VERSION = `0.34.3` as const
+  export const PKG_VERSION = `0.34.4` as const
 
   /** @internal */
   type id<type> = type
