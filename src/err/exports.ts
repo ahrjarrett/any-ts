bba272241c459fd3b5edbfcf7b0787a9536352e2
Andrew Jarrett <ahrjarrett@gmail.com>
2024-03-08 19:35:54 -0600

    feat: adds `Partial` (`Partial.strict`) to enforce no excess props on a partial

--- src/err/exports.ts
+++ src/err/exports.ts
@@ -1,4 +1,4 @@
 export type { Err, TypeError } from "./err"
 export { Case, Msg } from "./err"
-export type { enforce } from "./enforce"
+export type { enforce, Partial, Partial as partial } from "./enforce"
 export type { Catch } from "./catch"
