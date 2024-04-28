73804f6c1c26552bd1deb59014ea9f0cd2896e66
Andrew Jarrett <ahrjarrett@gmail.com>
2024-04-28 16:31:44 -0500

    feat: adds `object.filter`  and  `object.filterKeys`

--- src/object.ts
+++ src/object.ts
@@ -1,8 +1,88 @@
-export {
-  object_ as object
+import type { any } from "./any/exports"
+import type { some } from "./some"
+import type { eval } from "./evaluate/exports"
+
+export declare namespace object {
+  type filter = {
+    <A, B>(guard: any.typeguard<A, B>): <const T extends any.dict<A>>(object: T) => filter.values<T, B>
+    <A>(predicate: some.predicate<A>): <const T extends any.dict<A>>(object: T) => filter.values<T, A>
+    <A, B, const T extends any.dict<A>>(object: T, guard: any.typeguard<A, B>): filter.values<T, B>
+    <A, const T extends any.dict<A>>(object: T, predicate: some.predicate<A>,): filter.values<T, A>
+  }
+
+  type filterKeys = {
+    <A extends any.index, B extends any.index>(guard: any.typeguard<A, B>): <const T extends any.indexedBy<A>>(object: T) => filter.keys<T, B>
+    <A extends any.index>(predicate: some.predicate<A>): <const T extends any.indexedBy<A>>(object: T) => filter.keys<T, A>
+    <A extends any.index, B extends any.index, const T extends any.indexedBy<A>>(object: T, guard: any.typeguard<A, B>): filter.keys<T, B>
+    <A extends any.index, const T extends any.indexedBy<A>>(object: T, predicate: some.predicate<A>,): filter.keys<T, A>
+  }
 }
 
-type id<type> = type
+declare namespace filter {
+  type keys<t, bound extends any.index> = never |
+    eval<
+      & filter.keys.satisfy<t, bound>
+      & filter.keys.partiallySatisfy<t, bound>
+      & filter.keys.mightSatisfy<t, bound>
+    >
 
-interface object_ extends id<object> { }
-declare const object_: object_
+  namespace keys {
+    type satisfy<t, bound extends any.index> = never |
+      { -readonly [k in keyof t as filter.allSatisfy<k, bound> extends true ? k : never]: t[k] }
+    type partiallySatisfy<t, bound extends any.index> = never |
+      { -readonly [k in keyof t as filter.partiallySatisfy<k, bound> extends true ? k : never]: t[k] }
+    type mightSatisfy<t, bound extends any.index> = never |
+      { -readonly [k in keyof t as filter.mightSatisfy<k, bound> extends true ? k : never]: t[k] }
+  }
+
+  type values<t, bound> = never |
+    eval<
+      & filter.values.satisfy<t, bound>
+      & filter.values.partiallySatisfy<t, bound>
+      & filter.values.mightSatisfy<t, bound>
+    >
+
+  namespace values {
+    type satisfy<t, bound> = never |
+      { -readonly [k in keyof t as filter.allSatisfy<t[k], bound> extends true ? k : never]: t[k] }
+    type partiallySatisfy<t, bound> = never |
+      { -readonly [k in keyof t as filter.partiallySatisfy<t[k], bound> extends true ? k : never]+?: t[k] }
+    type mightSatisfy<t, bound> = never |
+      { -readonly [k in keyof t as filter.mightSatisfy<t[k], bound> extends true ? k : never]+?: filter.cast<t[k], bound> }
+  }
+
+  type cast<type, invariant, distribute = never>
+    = [distribute] extends [never]
+    ? (type extends invariant ? type : never) extends infer narrow
+    ? [narrow] extends [never] ? invariant
+    : narrow
+    : never
+    : type extends type ? filter.cast<type, invariant, never>
+    : never
+    ;
+
+  type allSatisfy<t, bound>
+    = (
+      bound extends bound
+      ? (t extends t & bound ? t : never) extends infer out
+      ? out
+      : never
+      : never
+    ) extends infer out
+    ? [t, out] extends [out, t] ? true
+    : false
+    : never
+    ;
+
+  type partiallySatisfy<t, bound>
+    = filter.allSatisfy<t, bound> extends true ? false
+    : [bound extends bound ? (t extends bound ? true : never) : never] extends [never] ? false
+    : true
+    ;
+
+  type mightSatisfy<t, bound>
+    = [t] extends [bound] ? never
+    : [t extends t ? (bound extends t ? true : never) : never] extends [never] ? false
+    : true
+    ;
+}
