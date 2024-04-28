bb95d5aaecd06a1c80e3d09a096329feb00f185a
Andrew Jarrett <ahrjarrett@gmail.com>
2024-04-28 17:20:03 -0500

    chore: exports internal `Fn*` modules

--- src/kind/exports.ts
+++ src/kind/exports.ts
@@ -13,6 +13,28 @@ export type {
   Scope,
   /** @internal */
   $$,
+  /** @internal */
+  Fn1,
+  /** @internal */
+  Fn10,
+  /** @internal */
+  Fn2,
+  /** @internal */
+  Fn3,
+  /** @internal */
+  Fn4,
+  /** @internal */
+  Fn5,
+  /** @internal */
+  Fn6,
+  /** @internal */
+  Fn7,
+  /** @internal */
+  Fn8,
+  /** @internal */
+  Fn9,
+  /** @internal */
+  cached,
 } from "./kind"
 
 export type {
