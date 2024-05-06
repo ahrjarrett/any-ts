1dc129b3e43be8fe58d6d4c357a8c319099dd08f
Andrew Jarrett <ahrjarrett@gmail.com>
2024-05-06 14:56:22 -0500

    chore: use `id` util in `Identity` type internally

--- src/identity.ts
+++ src/identity.ts
@@ -1,4 +1,4 @@
-export type identity<x> = x
+import type { id } from "./util";
 
 /* @ts-expect-error hush */
-export interface Identity<nonunion extends {} = {}> extends identity<nonunion> { }
+export interface Identity<nonunion extends {} = {}> extends id<nonunion> { }
