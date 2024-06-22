a7adb1e56d077246920d2acf8d1256ad49588787
Andrew Jarrett <ahrjarrett@gmail.com>
2024-06-22 18:18:17 -0500

    break: renames `match.finite*` to `match.finite.*` -- e.g. `match.finiteArray` -> `match.finite.array`

--- src/match/match.ts
+++ src/match/match.ts
@@ -1,32 +1,33 @@
+import type { some } from "../some/some.js";
 import type { any } from "../any/any.js"
 import type { never } from "../never/never.js"
 
 export type {
-  match_finiteArray as finiteArray,
-  match_finiteBoolean as finiteBoolean,
-  match_finiteKey as finiteKey,
-  match_finiteIndex as finiteIndex,
-  match_finiteLiteral as finiteLiteral,
-  match_finiteNumber as finiteNumber,
-  match_finiteString as finiteString,
-
-}
-
-export type {
-  match_record as record,
   match_emptyObject as emptyObject,
-  match_nonfiniteArray as nonfiniteArray,
-  match_strict as strict,
 }
 
-declare namespace match_strict {
+export declare namespace finite {
   export {
-    match_finiteBooleanStrict as finiteBoolean,
-    match_strictSubsetOf as subsetOf,
+    match_finiteBoolean as boolean,
+    match_finiteKey as key,
+    match_finiteIndex as index,
+    match_finiteLiteral as literal,
+    match_finiteNumber as number,
+    match_finiteString as string,
+    match_finiteObject as object,
+    match_finiteArray as array,
   }
 }
 
-type match_finiteArray<t>
+export declare namespace nonfinite {
+  export {
+    match_nonfiniteArray as array,
+    match_nonfiniteObject as record,
+    match_nonfiniteObject as object,
+  }
+}
+
+export type match_finiteArray<t>
   = [
     t extends any.array
     ? number extends t["length"] ? never
@@ -35,7 +36,7 @@ type match_finiteArray<t>
   ] extends [infer out extends any.array] ? out : never
   ;
 
-type match_nonfiniteArray<t>
+export type match_nonfiniteArray<t>
   = [
     t extends any.array
     ? number extends t["length"] ? t
@@ -44,63 +45,109 @@ type match_nonfiniteArray<t>
   ] extends [infer out] ? out : never
   ;
 
-type match_record<t>
-  = [t] extends [any.struct]
-  ? [t] extends [any.array]
-  ? never
-  : any.struct
-  : never
-  ;
-
-type match_emptyObject<t>
+export type match_emptyObject<t>
   = [t] extends [any.struct]
   ? [keyof t] extends [never] ? {}
   : never
   : never
   ;
 
-type match_finiteString<t>
+export type match_finiteString<t>
   = [t] extends [string]
   ? [string] extends [t] ? never
   : string
   : never
   ;
 
-type match_finiteNumber<t>
+export type match_finiteNumber<t>
   = [t] extends [number]
   ? [number] extends [t] ? never
   : number
   : never
   ;
 
-type match_finiteBoolean<t>
-  = [globalThis.Extract<t, boolean>] extends [infer out]
+export type match_finiteBoolean<t>
+  = [Extract<t, boolean>] extends [infer out]
   ? [boolean] extends [out] ? never
   : out
   : never
   ;
 
-type match_finiteBooleanStrict<t>
+export type match_finiteBooleanStrict<t>
   = [[t] extends [boolean] ? [boolean] extends [t] ? never : t : never] extends [infer out]
   ? out
   : never
   ;
 
-type match_finiteKey<t>
+export type match_finiteKey<t>
   = [t] extends [any.key]
   ? any.key extends t ? never
   : any.key
   : never
   ;
 
-type match_finiteIndex<t> = match_strict.subsetOf<t, any.index>
+export type match_finiteIndex<t>
+  = match_strictSubsetOf<t, any.index>
+  ;
 
-type match_strictSubsetOf<t, set>
+export type match_strictSubsetOf<type, set>
   = set extends set
-  ? [set extends t ? never : globalThis.Extract<t, set>] extends [infer out]
+  ? [set extends type ? never : Extract<type, set>] extends [infer out]
   ? out
   : never.close.inline_var<"out">
   : never.close.distributive<"set">
   ;
 
-type match_finiteLiteral<t> = match_finiteBoolean<t> | match_strict.subsetOf<t, any.key>
+export type match_finiteLiteral<t>
+  = match_finiteBoolean<t>
+  | match_strictSubsetOf<t, any.key>
+  ;
+
+export type match_nonfiniteObject<t, value = unknown>
+  = [t] extends [object]
+  ? [t] extends [any.array] ? never
+  : [string] extends [keyof t] ? { [x: string]: value }
+  : [number] extends [keyof t] ? { [x: number]: value }
+  : [symbol] extends [keyof t] ? { [x: symbol]: value }
+  : never
+  : never
+  ;
+
+export type match_nonfiniteStruct<t, value = unknown>
+  = [t] extends [object]
+  ? [t] extends [any.array]
+  ? [number] extends [t["length"]] ? any.array<value> : never
+  : [string] extends [keyof t] ? { [x: string]: value }
+  : [number] extends [keyof t] ? { [x: number]: value }
+  : [symbol] extends [keyof t] ? { [x: symbol]: value }
+  : never
+  : never
+  ;
+
+export type match_finiteStruct<t, value = unknown>
+  = [t] extends [object]
+  ? [t] extends [any.array]
+  ? [number] extends [t["length"]] ? never : any.array<value>
+  : [string] extends [keyof t] ? never
+  : [number] extends [keyof t] ? never
+  : [symbol] extends [keyof t] ? never
+  : { [x: any.key]: value }
+  : never
+  ;
+
+export type match_finiteObject<t, constraint extends object = {}>
+  = [t] extends [object]
+  ? [t] extends [any.array] ? never
+  : [string] extends [keyof t] ? never
+  : [number] extends [keyof t] ? never
+  : [symbol] extends [keyof t] ? never
+  : constraint
+  : never
+  ;
+
+export declare namespace strict {
+  export {
+    match_finiteBooleanStrict as finiteBoolean,
+    match_strictSubsetOf as subsetOf,
+  }
+}
